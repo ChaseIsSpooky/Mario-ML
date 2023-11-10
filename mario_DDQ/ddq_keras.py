0579c46699350cfa4e74e8419274e2fc16026b28
hovsam <49998896+hovsam@users.noreply.github.com>
2023-11-10 09:11:22 -0600

    Basic framework for ddq_keras.py and run.py for ddq

--- mario_DDQ/ddq_keras.py
+++ mario_DDQ/ddq_keras.py
@@ -1,133 +1,58 @@
-from keras.layers import Dense, Activation
-from keras.models import Sequential, load_model
-from keras.optimizers import Adam
+import random
 import numpy as np
+import gym
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
-class ReplayBuffer(object):
-    def __init__(self, max_size, input_shape, n_actions, discrete=False):
-        self.mem_size = max_size
-        self.mem_cntr = 0
-        self.discrete = discrete
-        self.state_memory = np.zeros((self.mem_size, input_shape))
-        self.new_state_memory = np.zeros((self.mem_size, input_shape))
-        dtype = np.int8 if self.discrete else np.float32
-        self.action_memory = np.zeros((self.mem_size, n_actions),dtype=dtype)
-        self.reward_memory = np.zeros(self.mem_size)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.float32)
+class DDQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95  # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.01
+        self.learning_rate = 0.001
+        self.model = self.build_model()
+        self.target_model = self.build_model()
 
-    def store_transitions(self, state, action, reward, state_, done):
-        index = self.mem_cntr % self.mem_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        if self.discrete:
-            actions = np.zeros(self.action_memory.shape[1])
-            actions[action] = 1.0
-            self.action_memory[index] = actions
-        else:
-            self.action_memory[index] = action
-        self.reward_memory[index] = reward
-        self.terminal_memory[index] = 1-int(done)
-        self.mem_cntr += 1
+    def build_model(self):
+        model = Sequential()
+        # Build your neural network architecture here
+        # For example:
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
 
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
-    def sample_buffer(self, batch_size):
-        max_mem = min(self.mem_cntr, self.mem_size)
-        batch = np.random.choice(max_mem, batch_size)
-        states = self.state_memory[batch]
-        new_states = self.new_state_memory[batch]
-        actions = self.action_memory[batch]
-        rewards = self.reward_memory[batch]
-        terminal = self.terminal_memory[batch]
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
 
-        return states, actions, rewards, new_states, terminal
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = self.target_model.predict(state)
+            if done:
+                target[0][action] = reward
+            else:
+                a = np.argmax(self.model.predict(next_state)[0])
+                t = self.target_model.predict(next_state)[0][a]
+                target[0][action] = reward + self.gamma * t
+            self.model.fit(state, target, epochs=1, verbose=0)
 
-def build_dqn(lr, n_actions, input_dims, fcl_dims, fc2_dims):
-    model = Sequential([
-                Dense(fcl_dims, input_shape=(input_dims,)),
-                Activation('relu'),
-                Dense(fc2_dims),
-                Activation('relu'),
-                Dense(n_actions)])
-    
-    model.compile(optimizer=Adam(lr=lr), loss='mse')
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
-    return model
+        self.target_model.set_weights(self.model.get_weights())
 
-
-class DDQNAgent1(object):
-    def __init__(self, alpha, gamma, n_actions, epsilon, batch_size,
-                 input_dims, epsilon_dec=0.996, epsilon_end=0.01,
-                 mem_size=1000000, fname='ddqn_model.h5', replace_target=100):
-        self.n_actions = n_actions
-        self.action_space = [i for i in range(self.n_actions)]
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.epsilon_dec = epsilon_dec
-        self.epsilon_min = epsilon_end
-        self.batch_size = batch_size
-        self.model = fname
-        self.replace_target = replace_target
-        self.memory = ReplayBuffer(mem_size, input_dims, n_actions, True)
-        self.q_eval = build_dqn(alpha, n_actions, input_dims, 256, 256)
-        self.q_targetl = build_dqn(alpha, n_actions, input_dims, 256, 256)
-
-    def remember(self, state, action, reward, new_state, done):
-        self.memory.store_transitions(state, action, reward, new_state, done)
-
-    def choose_action(self, state):
-        state = state[np.newaxis, :]
-        rand = np.random.random()
-        if rand < self.epsilon:
-            action = np.random.choice(self.action_space)
-        else:
-            actions = self.q_eval.predict(state)
-            action = np.argmax(actions)
-
-        return action
-    
-    def learn(self):
-        if self.memory.mem_cntr > self.batch_size:
-            state, action, reward, new_state, done = \
-                        self.memory.sample_buffer(self.batch_size)
-            action_values = np.array(self.action_space, dtype=np.int8)
-            action_indices = np.dot(action, action_values)
-
-            q_next = self.q_target.predict(new_state)
-            q_eval = self.q_eval.predict(new_state)
-
-            q_pred = self.q_eval.predict(state)
-
-            max_actions = np.argmax(q_eval, axis=1)
-
-            q_target = q_pred
-
-            batch_index = np.arange(self.batch_size, dtype=np.int32)
-
-            q_target[batch_index, action_indices] = reward + self.gamma*q_next[batch_index, max_actions.astype(int)]*done
-
-            _ = self.q_eval.fit(state, q_target, verbose=0)
-
-            self.epsilon = self.epsilon*self.epsilon_dec if self.epsilon > \
-                self.epsilon_min else self.epsilon_min
-            
-            if self.memory.mem_cntr % self.replace_target == 0:
-                self.update_network_parameters()
-
-    def update_network_parameters(self):
-        self.q_target.model.set_weights(self.q_eval.model.get_weights())
-
-    def save_model(self):
-        self.q_eval.save(self.model_file)
-
-    def load_model(self):
-        self.q_eval = load_model(self.model_file) 
-
-        if self.epsilon  <= self.epsilon_min:
-            self.update_network_parameters()
-            
-
-
-
-
-
-    
--- mario_DDQ/run.py
+++ mario_DDQ/run.py
@@ -1,47 +1,11 @@
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
-from ddq_keras import DDQNAgent1
 from utils import plotLearning
 import numpy as np
+import gym
+from ddq_keras import DDQNAgent
     
-if __name__ == '__main__':
-    env = gym_super_mario_bros.make('SuperMarioBros-v0')
-    env = JoypadSpace(env, COMPLEX_MOVEMENT)
-    ddqn_agent = DDQNAgent1(alpha=.0005, gamma= 0.99, n_actions=12, epsilon=1.0,
-                           batch_size=64,input_dims=8)
-    
-    n_games = 500
-    #ddqn_agent.load_model()
-    ddqn_scores = []
-    eps_history = []
-
-    
-    for i in range(n_games):
-        done = False
-        score = 0
-        observation = env.reset()
-        while not done:
-            action = ddqn_agent.choose_action(observation)
-            observation_,reward,done,info = env.step(action)
-            score += reward
-            ddqn_agent.remember(observation, action, reward, observation_, done)
-            observation = observation_
-            ddqn_agent.learn()
-        eps_history.append(ddqn_agent.epsilon)
-        ddqn_scores.append(score)
-
-        avg_score = np.mean(ddqn_scores[max(0, i-100):(i+1)])
-        print('episode ', i, 'score %.2f' % score, 'average score %.2f' % avg_score)
-
-        if i%10 == 0 and i > 0:
-            ddqn_agent.save_model()
-
-    filename = 'mario-ddqn.png'
-    x = [i+1 for i in range(n_games)]
-    plotLearning(x, ddqn_scores, eps_history, filename)
-
-   
 
 """     done = True
     for step in range(5000):
@@ -52,3 +16,37 @@ if __name__ == '__main__':
 
     env.close() """
 
+# Initialize the Mario environment
+env = gym.make('SuperMarioBros-1-1-v0')
+state_size = env.observation_space.shape[0]
+action_size = len(COMPLEX_MOVEMENT)  
+
+# Initialize DDQN agent
+agent = DDQNAgent(state_size, action_size)
+
+# Training parameters
+batch_size = 32
+num_episodes = 1000
+
+for episode in range(num_episodes):
+    state = env.reset()
+    print("state = ", state, "state_size = ", state_size)
+    state_size = env.observation_space.shape
+    state = np.reshape(state, [1, state_size])
+
+    total_reward = 0
+    for time in range(500): 
+        action = agent.act(state)
+        next_state, reward, done, _ = env.step(action)
+        next_state = np.reshape(next_state, [1, state_size])
+        agent.remember(state, action, reward, next_state, done)
+        state = next_state
+
+        total_reward += reward
+
+        if done:
+            print("Episode: {}/{}, Total Reward: {}, Epsilon: {:.2}".format(episode + 1, num_episodes, total_reward, agent.epsilon))
+            break
+
+    if len(agent.memory) > batch_size:
+        agent.replay(batch_size)
